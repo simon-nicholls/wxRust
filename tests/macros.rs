e81d6d0107528b52c8d7f2fcf723541b85af176d
Simon Peter Nicholls <simon@mintsource.org>
2014-10-31 13:18:03 +0100

    remove bare raw pointers in macros

--- tests/macros.rs
+++ tests/macros.rs
@@ -3,7 +3,7 @@
 pub macro_rules! wxApp(
     ($f: ident) => (
         #[start]
-        fn start(argc: int, argv: **u8) -> int {
+        fn start(argc: int, argv: *const *const u8) -> int {
 
             use libc::c_void;
 
@@ -14,7 +14,7 @@ pub macro_rules! wxApp(
 
             native::start(argc, argv, proc() {
                 let closure = Closure::new($f as *mut c_void, nullptr);
-                let args: Vec<*i32> = Vec::new();
+                let args: Vec<*mut i32> = Vec::new();
                 RustApp::initializeC(&closure, args.len() as i32, args.as_ptr() as *mut *mut i8);
             })
         }
